bb02e7a2ec465ebfbcb83a6ba68ae3b3ef6ec13f
William Dahl <williamad90@gmail.com>
2021-03-19 12:11:21 +0100

    think the files were missing

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] =[
+  {id: 11, name: 'Dr Nasty'},
+  {id: 12, name: 'Narcos'},
+  {id: 13, name: 'Bomboon'},
+  {id: 14, name: 'Cliteritas'},
+  {id: 15, name: 'Smegma'},
+  {id: 16, name: 'CondomMan'},
+  {id: 17, name: 'DynaMama'},
+  {id: 18, name: 'Dr lowIQ'},
+  {id: 19, name: 'Smegarella'},
+  {id: 20, name: 'Tornada'}
+]
