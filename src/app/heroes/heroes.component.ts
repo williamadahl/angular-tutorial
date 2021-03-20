adea380d868a4be036e155d68486270d69ad9ccf
William Dahl <williamad90@gmail.com>
2021-03-20 15:45:25 +0100

    added a hero detail route, deleted hero detail in the heroes component, dashboard now navigates via a parametarized dashboard route, removed onSelected in heroes component html and the actuall component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,19 +11,13 @@ import { MessageService} from "../message.service";
 export class HeroesComponent implements OnInit {
 
   heroes : Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(
-    private heroService : HeroService,
-    private messageService: MessageService
+    private heroService : HeroService
   ) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes );
